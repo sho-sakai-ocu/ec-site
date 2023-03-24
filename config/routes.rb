29e5012e48d869d710c1f9601c1346b2b5ee4cb5
EC2 Default User <ec2-user@ip-172-31-93-118.ec2.internal>
2023-03-24 20:20:26 +0000

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'homes#top'
     resources :items, except: [:destroy]
+    resources :customers, only: [:index, :show, :edit, :update]
   end
 
 
