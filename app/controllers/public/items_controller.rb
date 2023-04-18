dd4a0219e0719c24883f8ceb2539b3d8d411e447
sho-sakai-ocu <a19sa015@st.osaka-cu.ac.jp>
2023-04-18 09:10:18 +0900

    commit

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,6 +1,12 @@
 class Public::ItemsController < ApplicationController
   def index
-    @items = Item.all
+    @genres = Genre.all
+    @genre_id = params[:id]
+    if @genre_id.present?
+      @items = Item.where(genre_id: @genre_id)
+    else
+      @items = Item.all
+    end
   end
 
   def show
